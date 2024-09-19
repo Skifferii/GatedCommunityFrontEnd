e8bb7dc42d8143d4f1058fd2ab1d505d392d23b7
Evgen Grushchenko <evgen.tm959@gmail.com>
2024-09-19 19:31:41 +0200

    Evgen001

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import OfferedServicesList from './components/offered-services-list/OfferedServicesList'
+
+function App() {
+
+
+  return (
+    <>
+      <OfferedServicesList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/offered-services-list/OfferedServicesList.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function OfferedServicesList(){
+    const [services, setServices] = useState([]);
+    async function fetchServices(){
+       const res = await fetch("/api/offered-services"); 
+       const obj =  await res.json();
+       setServices(obj)
+    }
+
+    useEffect(()=>{
+        fetchServices();
+    },[])
+
+    return <>
+        <ul>
+            {services.map((service: {title: string, id: number, description: string})=> 
+            <li key={service.id}>{service.title}--{service.description}</li>)}
+        </ul>
+    </>
+}
+
+export default OfferedServicesList
