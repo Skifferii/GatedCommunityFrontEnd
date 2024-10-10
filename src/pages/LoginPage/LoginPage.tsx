cd08df487fabbf3b13ee817bee9d20bbf73040d9
Skiferii <dolzhenkoandriy@gmail.com>
2024-10-10 20:41:41 +0200

    adreses+styles

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -44,6 +44,7 @@ const LoginPage: React.FC = () => {
           <label>Username:</label>
           <input
             type="text"
+            placeholder="Имя пользователя"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
@@ -53,6 +54,7 @@ const LoginPage: React.FC = () => {
           <label>Password:</label>
           <input
             type="password"
+            placeholder="Пароль"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
